7cf750691b63d5d8b388b6584bf421476361d9ad
xaviervasquez21 <xavier.vasquez@ucuenca.edu.ec>
2024-09-26 16:28:57 -0500

    Ultimo cambio con tercer cursor

--- sketch.js
+++ sketch.js
@@ -1,16 +1,29 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    //frameRate(4);
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    frameRate(60);
     background(0);
     console.log("hola");
     rectMode(CENTER);
 }
 
 function draw(){
-    background(random(30, 90), random(20,255),0);
-    fill (mouseY, 90, 20);
-    circle(mouseX + 50, mouseY - 50, mouseX);
+    background(random(30, 90), random(20,255),0,10);
+
+    //no relleno solo coloco el no
+    //strokeWeight(5);
+    //noFill();
+    //stroke(255, 0, 0);
+    //circle(mouseX + 50, mouseY - 50, mouseX);
+    
     //La sgiuiente linea da color al cuadrado
-    fill(200, 90, 30)
-    rect(mouseX,mouseY,100,100);
+    //strokeWeight(1);
+    //stroke(255, 0, 0);
+    //fill(200, 90, 30);
+    //rect(mouseX,mouseY,100,100);
+
+    // Draw the triangle.
+    fill(228, 0, 224);
+    noStroke();
+    rotateY(frameCount * 0.10);
+    triangle(mouseX, mouseY, 30, 40, 50, 80);
 }
