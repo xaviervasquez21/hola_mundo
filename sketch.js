c3824eb1c2fdedf06c358afcf8f447b452ec16d6
xaviervasquez21 <xavier.vasquez@ucuenca.edu.ec>
2024-09-26 15:27:23 -0500

    Se creo un cursor bien bonito

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
-function setup(){
+function setup() {
     createCanvas(windowWidth, windowHeight);
+    //frameRate(4);
     background(0);
+    console.log("hola");
+    rectMode(CENTER);
 }
 
 function draw(){
-    
+    background(random(30, 90), random(20,255),0);
+    fill (mouseY, 90, 20);
+    circle(mouseX + 50, mouseY - 50, mouseX);
+    //La sgiuiente linea da color al cuadrado
+    fill(200, 90, 30)
+    rect(mouseX,mouseY,100,100);
 }
